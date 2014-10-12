b752d704685f45dc9eae9c0cde83717e39aff603
ohall <ohall@grassroots-tech.com>
2014-10-12 10:55:41 -0400

    grunt init

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Oakley Hall on 10/12/14.
+ */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    });
+
+
+
+    // Default task(s).
+    grunt.registerTask('default', []);
+
+};
