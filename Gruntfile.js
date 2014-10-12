61cd90f1e4bd0b90aa1d0ef1652bbeb49e973dcd
ohall <ohall@grassroots-tech.com>
2014-10-12 11:30:41 -0400

    grunt task for launching app

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,37 @@
  */
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-execute');
+    grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-open');
+
+    require('load-grunt-tasks')(grunt);
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concurrent: {
+            scores: ['execute', 'serve', 'open']
+        },
+        execute: {
+            target: {
+                src: ['scores.js']
+            }
+        },
+        serve: {
+            path: '/Users/oakley/Documents/projects/hapiscores/app/',
+            options: {
+                port: 9000
+            }
+        },
+        open : {
+            dev: {
+                path: 'http://localhost:9000/app/index.html'
+            }
+        }
+
     });
 
-
-
     // Default task(s).
-    grunt.registerTask('default', []);
-
+    grunt.registerTask('default', ['concurrent:scores']);
 };
